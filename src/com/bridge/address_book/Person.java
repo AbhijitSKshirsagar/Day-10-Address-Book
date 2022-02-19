e680c0261834fcf12b2a8e350dd59605ef2a151d
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-20 00:41:36 +0530

    added: address bbok

--- src/com/bridge/address_book/Person.java
+++ src/com/bridge/address_book/Person.java
@@ -1,9 +1,16 @@
 package com.bridge.address_book;
 
+import java.util.Scanner;
+
 public class Person {
-       static Contact [] personDetails = new Contact[10];
+	static Scanner sc = new Scanner(System.in);
+	static String firstName,lastName,address,state,email;
+	static int zip;
+	static long phoneNumber;
+    static Contact [] personDetails = new Contact[10];
+
 	
-	   public static void main(String args[]) {
+	public static void main(String args[]) {
 		
 		Contact person1 = new Contact("Raj","Kumar","Nagar","MH",35,8620,"raj@gmail.com");
 		Contact person2 = new Contact("Shashi","Rao","Chowk","TD",98,9736,"shashi@yahoo.com");
@@ -11,10 +18,40 @@ public class Person {
 	    personDetails[0]=person1;
 	    personDetails[1]=person2;
 	    personDetails[2]=person3;
-	    
 	    System.out.println("Created contact list is");
 	    for(int i = 0; i < 3;i++) {
 		   System.out.println(personDetails[i]);
 	    }
+	    operations();
+	}
+	public static void operations() {
+		int j = 0;
+	    System.out.println("Enter your choice");
+	    System.out.println("1)Add contact");
+	    int choice = sc.nextInt();
+	    switch(choice) {
+	    	case 1:
+	    			System.out.println("Enter details of person");
+		    		System.out.println("Add first name");
+		    		firstName = sc.next();
+		    		System.out.println("Add last name");
+		    		lastName= sc.next();
+		    		System.out.println("Add address");
+		    		address = sc.next();
+		    		System.out.println("Add state");
+		    		state= sc.next();
+		    		System.out.println("Add zip");
+		    		zip = sc.nextInt();
+		    		System.out.println("Add phone number");
+		    		phoneNumber = sc.nextLong();
+		    		System.out.println("Add email");
+		    		email = sc.next();
+		    		Contact person4 = new Contact(firstName,lastName,address,state,zip,phoneNumber,email);
+		    		personDetails[3]=person4;
+		    		j++;
+			 for(int i = 0; i < 3 + j;i++) {
+				   System.out.println(personDetails[i]);
+			 }
+		}
 	}
 }
